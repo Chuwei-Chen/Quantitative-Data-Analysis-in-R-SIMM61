32016a68ddd4e84bdf243a28e45638b507812045
Chuwei <95743162+Chuwei-Chen@users.noreply.github.com>
2022-02-01 19:36:20 +0100

    Add files via upload

--- AA4.R
+++ AA4.R
@@ -4,21 +4,67 @@ library(semptools)
 library(tidyverse)
 library(CompQuadForm)
 library(ICS)
-
-install.packages("psychTools")
 library(psychTools)
+
+#import dataset
 my_data = holzinger.swineford
 
+#specify the SEM model
 modelA<-'
 visual=~ t01_visperc+ t02_cubes+ t03_frmbord+t04_lozenges
 verbal=~ t06_paracomp+t07_sentcomp+t09_wordmean
 speed=~ t10_addition+t12_countdot+t13_sccaps
 
 '
+
+#fit the model
 fit <- sem(modelA, data = my_data)
-plot = semPaths(fit, label.scale=F, nCharNodes = 8,
-                sizeMan2=3.5, sizeMan=10, asize=3, edge.color="black", residuals = F, fixedStyle = 1)
+#visualization
+semPaths(fit)
+
 summary(fit)
 
 #check multivariate normality
 mvnorm.kur.test(my_data[,c("t01_visperc", "t02_cubes", "t03_frmbord", "t04_lozenges","t06_paracomp","t07_sentcomp","t09_wordmean","t10_addition","t12_countdot","t13_sccaps")])
+mvnorm.skew.test(my_data[,c("t01_visperc", "t02_cubes", "t03_frmbord", "t04_lozenges","t06_paracomp","t07_sentcomp","t09_wordmean","t10_addition","t12_countdot","t13_sccaps")])
+
+#solution with robust SE and test statistics
+fit_MLM <- sem(modelA, data = my_data, estimator = "MLM")
+summary(fit_MLM, fit.measures = T)
+
+#task 2:build modelB in case of t10 and t12 has similar structure
+modelB<-'
+visual=~ t01_visperc+ t02_cubes+ t03_frmbord+t04_lozenges
+verbal=~ t06_paracomp+t07_sentcomp+t09_wordmean
+speed=~ t10_addition+t12_countdot+t13_sccaps
+t10_addition~~t12_countdot
+
+'
+
+fit2 <- sem(modelB, data = my_data)
+
+semPaths(fit2,whatLabels = "std")
+
+summary(fit2)
+
+#solution with robust SE and test statistics
+fit2_MLM <- sem(modelB, data = my_data, estimator = "MLM")
+summary(fit2_MLM, fit.measures = T)
+semPaths(fit2_MLM, whatLabels = "std")
+
+#compare model A and model B
+anova(fit, fit2)
+
+#mediation model
+model_mediation = "
+t13_sccaps ~ c*t01_visperc + b*t12_countdot
+t12_countdot ~ a*t01_visperc
+
+indirect := a*b
+
+total := c + (a*b)
+"
+fit_mediation = sem(model_mediation, data = my_data)
+semPaths(fit_mediation, fixedStyle = 1, label.scale=F, nCharNodes = 0,
+         sizeMan2=5, sizeMan=15, asize=3, edge.label.cex = 1)
+summary(fit_mediation)
